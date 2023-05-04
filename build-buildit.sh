d573daf059e9df0e3d14b12032146fe78b55f46a
Ajay Brahmakshatriya <ajaybr@mit.edu>
2023-05-04 18:13:38 -0400

    Removed compiling deps

--- build-buildit.sh
+++ build-buildit.sh
@@ -1,5 +1,4 @@
 cd buildit
-make -C deps/libelfin
 
 make BUILD_DIR=$(pwd)/build -j$(nproc)
 make BUILD_DIR=$(pwd)/build_var_names LIBUNWIND_PATH=/libunwind/install/usr/local RECOVER_VAR_NAMES=1 -j$(nproc) TRACER_USE_LIBUNWIND=1
